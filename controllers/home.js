163837e500c2c31c604b3a5fd9c85e5c7fa77ed4
Ervin Natareno <ean1207@gmail.com>
2024-02-03 15:15:07 -0800

    Added Feature: Home Page

--- controllers/home.js
+++ controllers/home.js
@@ -2,5 +2,13 @@ module.exports = {
     getIndex: (req, res) => {
       res.render("index.ejs");
     },
+    getAbout: (req, res) => {
+      try {
+        res.render("about.ejs");
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      }
+    },
   };
   
